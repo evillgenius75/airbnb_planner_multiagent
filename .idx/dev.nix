2fd46d7cd274d244a5410ca99392dd0bcd7925cf
eddiejv <eddiejv@google.com>
2025-08-12 16:20:00 +0000

    update nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -2,13 +2,12 @@
 # see: https://firebase.google.com/docs/studio/customize-workspace
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
-  channel = "stable-24.05"; # or "unstable"
+  channel = "unstable"; # or "unstable"
 
   # Use https://search.nixos.org/packages to find packages
   packages = [
     # pkgs.go
     # pkgs.python313
-    # pkgs.python313Packages.pip
     pkgs.nodejs_20
     # pkgs.nodePackages.nodemon
     pkgs.python313
